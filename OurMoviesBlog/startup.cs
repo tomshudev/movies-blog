aa217f30fea248f71c48915fee38482c9d7ce865
Sagie <Sagie012@gmail.com>
2019-09-11 19:29:51 +0300

    Change Twitter Consumer ID

--- OurMoviesBlog/startup.cs
+++ OurMoviesBlog/startup.cs
@@ -10,7 +10,7 @@ namespace OurMoviesBlog
 {
     public class Startup
     {
-        private const string TWITTER_CONSUMER_ID = "97segLtHcO97zISHMh6VPNKOQ";
+        private const string TWITTER_CONSUMER_ID = "g4vIIbPIL9f4d7JKQMlcMgr1c";
         private const string TWITTER_CONSUMER_SECRET = "rQj57YltiOWNuEdR2gXjPyeIoyd4cbBK06YroUme3U1ZJqXxzN";
         private const string TWITTER_ACCESS_TOKEN = "1058689648057311232-oKh4R0EUN2KGQFLjYocmUIhZTf7Rd4";
         private const string TWITTER_ACCESS_TOKEN_SECRET = "PQ0YSBhJw8yBggJXQURHsSdiuRswQiZLMxxk6q1XT4Kld";

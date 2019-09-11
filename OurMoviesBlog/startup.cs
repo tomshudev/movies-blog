37431fd7a3eadd841d6dd9a78b68da0d2341480c
Sagie <Sagie012@gmail.com>
2019-09-11 19:32:14 +0300

    Change Twitter Access Token

--- OurMoviesBlog/startup.cs
+++ OurMoviesBlog/startup.cs
@@ -12,7 +12,7 @@ namespace OurMoviesBlog
     {
         private const string TWITTER_CONSUMER_ID = "g4vIIbPIL9f4d7JKQMlcMgr1c";
         private const string TWITTER_CONSUMER_SECRET = "9QNHRjCMWFBFmluClgIkcZhoQi8ytSnC6oaac8xIzOYSxKE4dZ";
-        private const string TWITTER_ACCESS_TOKEN = "1058689648057311232-oKh4R0EUN2KGQFLjYocmUIhZTf7Rd4";
+        private const string TWITTER_ACCESS_TOKEN = "1171060325774499840-HuWChy7EvZ3CiAbovggmbl3ZH4dJir";
         private const string TWITTER_ACCESS_TOKEN_SECRET = "PQ0YSBhJw8yBggJXQURHsSdiuRswQiZLMxxk6q1XT4Kld";
 
         public void Configuration(IAppBuilder app)

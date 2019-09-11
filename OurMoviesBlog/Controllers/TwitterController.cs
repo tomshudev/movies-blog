c4dfb1bee60b5d884803db3f51bbfd8f31712ce7
Sagie <Sagie012@gmail.com>
2019-09-11 19:31:45 +0300

    Change Twitter Consumer SECRET

--- OurMoviesBlog/Controllers/TwitterController.cs
+++ OurMoviesBlog/Controllers/TwitterController.cs
@@ -11,7 +11,7 @@ namespace OurMoviesBlog.Controllers
     public class TwitterController : Controller
     {
         private const string TWITTER_CONSUMER_ID = "g4vIIbPIL9f4d7JKQMlcMgr1c";
-        private const string TWITTER_CONSUMER_SECRET = "rQj57YltiOWNuEdR2gXjPyeIoyd4cbBK06YroUme3U1ZJqXxzN";
+        private const string TWITTER_CONSUMER_SECRET = "9QNHRjCMWFBFmluClgIkcZhoQi8ytSnC6oaac8xIzOYSxKE4dZ";
         private const string TWITTER_ACCESS_TOKEN = "1058689648057311232-oKh4R0EUN2KGQFLjYocmUIhZTf7Rd4";
         private const string TWITTER_ACCESS_TOKEN_SECRET = "PQ0YSBhJw8yBggJXQURHsSdiuRswQiZLMxxk6q1XT4Kld";
 
